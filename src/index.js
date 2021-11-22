c41b96be57f1a8553c9d87638c79c227ef9bfecc
nanfeiyan <32266005+nanfeiyan123@users.noreply.github.com>
2021-11-22 16:25:09 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import App from './App'
-import { Provider } from 'react-redux'
-import store from '@/redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "@/redux";
 
-import '@/style/reset.less'
-import '@/assets/iconfont/iconfont.css'
+import "@/style/reset.less";
+import "@/assets/iconfont/iconfont.css";
 
-import hljs from 'highlight.js'
-import javascript from 'highlight.js/lib/languages/javascript'
-import 'highlight.js/styles/atom-one-light.css'
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
+import "highlight.js/styles/atom-one-light.css";
 
-import '@/style/index.less'
+import "@/style/index.less";
 
-hljs.registerLanguage('javascript', javascript)
-
-const render = Component => {
+hljs.registerLanguage("javascript", javascript);
+console.log("ddd");
+const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <Provider store={store}>
         <Component />
       </Provider>
     </AppContainer>,
-    document.getElementById('root')
-  )
-}
+    document.getElementById("root")
+  );
+};
 
-render(App)
+render(App);
 
 // ReactDOM.render(<App />, document.getElementById('root'))
 
 if (module.hot) {
-  module.hot.accept('./App', () => {
-    render(App)
-  })
+  module.hot.accept("./App", () => {
+    render(App);
+  });
 }
